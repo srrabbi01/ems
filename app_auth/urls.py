adfb86fd423414ca4ce56d8ec1e935b8a9cc6782
srrabbi01 <srrabbi01@gmail.com>
2022-04-28 10:36:24 +0600

    pass_reset fix

--- app_auth/urls.py
+++ app_auth/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name = 'logout'),
     path('registration/', views.registration_view, name = 'registration'),
     path("password_reset/", views.password_reset_request, name="password_reset"),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='app_auth/password_reset/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='app_auth/password_reset/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='app_auth/password_reset/password_reset_complete.html'), name='password_reset_complete'),
 ]
